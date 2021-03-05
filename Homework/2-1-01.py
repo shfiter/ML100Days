734267d3fece4b5d9cbf596c09230cd7b8adad1b
shfiter <65352699+shfiter@users.noreply.github.com>
2021-03-05 11:35:14 +0800

    2021/03/05

---
+++ Homework/2-1-01.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import numpy as np
+
+
+# In[13]:
+
+
+a=np.arange(0, 21, 1)
+a
+
+
+# In[14]:
+
+
+a[2:-1:2]
+
+
+# In[15]:
+
+
+a[3:-1:3]
+
